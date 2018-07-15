5e994ed909523f45609e6749c3f93cb9a3abe044
Marko Poikkimäki <marko_poikkimaki@hotmail.com>
2018-07-15 11:27:14 +0200

    Episode 5

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -14,12 +14,30 @@
         </div>
 
         <div class="row">
-            <div class="col-md-6 col-md-offset-2">
+            <div class="col-md-10 col-md-offset-2">
                 @foreach($thread->replies as $reply)
                     @include('threads.reply')
                 @endforeach
             </div>
         </div>
+        @if(auth()->check())
+            <div class="row">
+                <div class="col-md-10 col-md-offset-2">
+                    <h5>Reply:</h5>
+                    <form method="post" action="{{ '/'. $thread->path() . '/replies' }}">
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                            <textarea name="body" id="body" placeholder="Have something to say?" rows="5" class="form-control"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-default" >Post</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        @else
+            <p class="text-center" >Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion</p>
+        @endif
     </div>
 @endsection
 
